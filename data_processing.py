838c35c4ef2bece5f3c17e360c6c1af6f72e670e
b.markiv <b.markiv@ruitenburg.nu>
2024-03-08 15:54:40 +0100

    -created weekly data

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,27 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
+data = pd.read_csv("grouped_data.csv")
 
-def prepare_data():
+data = data[data["effective_date"] <= '2023-12-31']
 
-    data = pd.read_excel("example_data/transaction_data_sample.xlsx")
-    output = {}
-    for type in data["Transaction_Type"]:
-        output[type] = data[data["Transaction_Type"] == type]
+grouped = data.groupby(["company_id", "Classification"])[
+    "amount"].agg(["mean", "count", "std"]).reset_index()
 
-    return output
+# Cretae weekly data
+data['effective_date'] = pd.to_datetime(data['effective_date'])
+data["week"] = data["effective_date"].dt.isocalendar().week
+data["year"] = data["effective_date"].dt.isocalendar().year
+
+weekly_data = data.groupby(["company_id", "Classification", "week", "year"])[
+    "amount"].sum().reset_index()
+
+example = weekly_data[(weekly_data["company_id"] == 248552) & (
+    weekly_data["Classification"] == "Operational Revenue")]
+
+example = example[["week", "year", "amount"]]
+example.index = example["week"].astype(str) + example["year"].astype(str)
+example = example.drop(columns=["week", "year"])
+
+plt.plot(example)
+plt.show()
