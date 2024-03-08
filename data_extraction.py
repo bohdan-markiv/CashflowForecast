d48a9b7f2a4245dcba7d88c9e1bc51fe3076de00
b.markiv <b.markiv@ruitenburg.nu>
2024-03-08 14:13:39 +0100

    -changed something

--- data_extraction.py
+++ data_extraction.py
@@ -1,31 +1,47 @@
 import pandas as pd
 import numpy as np
-
+from datetime import datetime, timedelta
 
 data = pd.read_csv("C:/Users/b.markiv/Downloads/full_data_set.csv")
 revenues = pd.read_csv("revenues.csv")
 
-data = data[data["company_id"].isin([27934117, 262866, 24872290, 18165120, 22205603, 11036957, 2199709,
-                                     8922050, 18305616, 2807402, 9646170, 11745239, 23673416,
-                                     16075016, 10374661, 18117434, 30879313, 26390251, 5645364,
+data = data[data["company_id"].isin([27934117, 262866, 24872290, 22205603, 11036957, 2199709,
+                                     8922050, 18305616, 2807402, 9646170, 11745239, 5645364,
                                      27233208, 9296446, 397293, 16862710, 10209459,
-                                     15878443, 1559829, 1628862])]
-revenues = revenues[revenues["Division"].isin([2163245, 999296, 2659781, 556104, 236141, 1555285, 2403092, 925373,
-                                               1426111, 2850605, 1122522, 1385214, 3138419, 3128238, 3383896,
-                                               2111655, 2781225, 375035, 365180, 3358248,
-                                               3646589, 2667341, 2391738, 1186317, 3469108, 2377423, 250980])]
-revenues.to_csv("filtered_revenue.csv")
+                                     8685703, 30940084, 17200872, 25061843, 31927532, 1172922,
+                                     248552, 25265199, 30549617])]
 
+revenues = revenues[revenues["Division"].isin([2163245, 999296, 2659781, 236141, 1555285, 2403092, 925373,
+                                               1426111, 2850605, 1122522, 1385214,
+                                               2111655, 2781225, 375035, 365180, 3358248,
+                                               1186317, 3388580, 1890439, 551084, 2143, 1235235,
+                                               1006367, 2465565, 1410815, 2448229])]
+revenues.to_csv("filtered_revenue.csv")
 
 def anonymyse_ids(df, name):
     df[name] = (df[name] * 2) + 1810
     return df
 
+def convert_date(date):
+    # Check if the value is an integer, which would imply it's an Excel serial date
+    try:
+        if isinstance(date, int) or (isinstance(date, str) and date.isdigit()):
+            excel_start_date = datetime(1899, 12, 30)
+            delta = timedelta(days=int(date))
+            return (excel_start_date + delta).date()
+        else:
+            # Parse the date string to datetime
+            return pd.to_datetime(date).date()
+    except Exception as e:
+        return pd.NaT 
+
 # data = anonymyse_ids(data, "company_id")
 
 
+
+
 data["ledger_id"] = data["ledger_id"].astype(str)
-data['effective_date'] = pd.to_datetime(data['effective_date'])
+data['effective_date'] = data['effective_date'].apply(convert_date)
 data = data.drop(columns=["type"])
 
 mask = (
@@ -43,7 +59,7 @@ mask = (
     data['ledger_id'].str.startswith('82')
 )
 data = data[mask]
-data = data[data["effective_date"] >= '2022-01-01']
+data = data[data["effective_date"] >= datetime.strptime('2022-01-01', '%Y-%m-%d').date()]
 
 data["AR/AP"] = np.where(data["ledger_id"].str.startswith("8"),
                          "Account Receivable", "Account Payable")
@@ -67,7 +83,32 @@ grouped_data2 = data.groupby(["company_id", "Classification"])[
     "amount"].sum().reset_index()
 overview = grouped_data1.merge(
     grouped_data2, on=["company_id", "Classification"])
-overview.to_csv("grouped_data.csv")
 
+overview.to_csv("overview_data.csv")
+
+grouped_on_ar_ap = data.groupby(["company_id", "AR/AP"])[
+    "amount"].agg(["mean", "count", "std"]).reset_index()
+
+wide_df = grouped_on_ar_ap.pivot_table(index='company_id', 
+                         columns='AR/AP', 
+                         values=['mean', 'count', "std"]).reset_index()
+
+# Flatten the MultiIndex in columns
+wide_df.columns = ['_'.join(col).strip() if col[1] else col[0] for col in wide_df.columns.values]
+
+wide_df.rename(columns={
+    'company_id_': 'company_id', 
+    'mean_Account Payable': 'AP_mean', 
+    'mean_Account Receivable': 'AR_mean', 
+    'count_Account Payable': 'AP_count', 
+    'count_Account Receivable': 'AR_count',
+    'std_Account Payable': 'AP_std', 
+    'std_Account Receivable': 'AR_std',
+}, inplace=True)
+
+wide_df = anonymyse_ids(wide_df, "company_id")
+grouped_data = data.groupby(["company_id", "Classification", "effective_date"])[
+    "amount"].sum().reset_index()
+grouped_data.to_csv("grouped_data.csv")
 
 data.to_csv("classified_data.csv")
--- graphs.py
+++ graphs.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 revenue = pd.read_csv("filtered_revenue.csv")
 revenue["rev_in_ks"] = revenue["Revenue 2023"]/1000
@@ -8,4 +9,8 @@ revenue["rev_in_ks"].describe()
 
 fig = plt.figure(figsize=(10, 7))
 plt.boxplot(revenue["rev_in_ks"])
+plt.show()
+
+plt.hist(revenue["rev_in_ks"], color = 'blue', edgecolor = 'black',
+         bins = int(180/5))
 plt.show()
